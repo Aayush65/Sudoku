3d536a4c4bc7e308c00b2673997afb246823389c
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-06-21 12:12:40 +0530

    Made Site responsive and Changed color theme (default to white)

--- src/App.tsx
+++ src/App.tsx
@@ -54,12 +54,13 @@ function App() {
 		setIsFilling(true);
 		handleReset();
 		
-		// Aborting after 1.5s
+		// Aborting after timeoutTime ms
+		const timeoutTime = 3000
 		const abortRequest = new AbortController();
 		const abortTimeOut = setTimeout(() => {
 			abortRequest.abort(); 
 			setIsFilling(false);
-		}, 2500);
+		}, timeoutTime);
 
 		const apiCall = await fetch('https://sugoku.onrender.com/board?difficulty=medium', abortRequest);
 		clearTimeout(abortTimeOut);
@@ -88,24 +89,25 @@ function App() {
 	}
 
 	return (
-		<>
-			<div className='border-white border-4 w-[30rem] h-[30rem] flex flex-wrap justify-center gap-3 p-2 bg-[#827b7b]'>
+		<div className='flex flex-col justify-center items-center'>
+			<h1 className='mb-10 text-2xl md:text-5xl font-black text-center'>It's Mind Sharpening Time!!!</h1>
+			<div className='border-black border-8 md:border-4 w-[21rem] h-[21rem] md:w-[30rem] md:h-[30rem] flex flex-wrap gap-3 p-1 md:p-2 bg-[#3f3838]'>
 				{grid.map((row, r) => (
-					<div key={r} className='w-[9rem] h-[9rem] flex justify-center flex-wrap'>
+					<div key={r} className='w-[6rem] h-[6rem] md:w-[9rem] md:h-[9rem] flex justify-center flex-wrap'>
 						{row.map((col, c) => (
-							<div key={c} className='w-[3rem] h-[3rem] border-white border-2'>
-								<input disabled={fixed[r][c]} value={col ? col : ''} className={`w-full h-full text-center text-[white] focus:bg-[#4f4f4f] hover:cursor-pointer focus:hover:cursor-pointer ${inValids[r][c] && !fixed[r][c] ? 'bg-[#dc2d2d]' : ''} ${solving ? 'cursor-not-allowed' : ''}`} onChange={(e) => { handleChange(r, c, parseInt(e.target.value.slice(-1)) || 0) }} />
+							<div key={c} className='w-[2rem] h-[2rem] md:w-[3rem] md:h-[3rem] border-gray-800 border-[1px] md:border-2'>
+								<input disabled={fixed[r][c]} value={col ? col : ''} className={`w-full h-full text-center text-black focus:bg-[#e7dfdf] hover:cursor-pointer focus:hover:cursor-text ${inValids[r][c] && !fixed[r][c] ? 'bg-[#dc2d2d] text-white focus-within:text-black' : ''} ${solving ? 'cursor-not-allowed' : ''} disabled:bg-[#b5b9b9]`} onChange={(e) => { handleChange(r, c, parseInt(e.target.value.slice(-1)) || 0) }} />
 							</div>
 						))}
 					</div>
 				))}
 			</div>
-			<div className='flex justify-around'>
-				<button type='button' disabled={isFilling} onClick={handleReset} className={`bg-white text-[#242424] p-3 rounded-md mt-4 active:scale-110 ${isFilling ? 'bg-[#646464]' : ''}`}>Reset</button>
-				<button type='button' disabled={isFilling} onClick={handleFill} className='bg-white text-[#242424] p-3 rounded-md mt-4 active:scale-110'>{isFilling ? 'Filling...' : 'Fill'}</button>
-				<button type='button' disabled={isFilling} onClick={handleSubmit} className={`bg-white text-[#242424] p-3 rounded-md mt-4 active:scale-110 ${isFilling ? 'bg-[#646464]' : ''}`}>{solving ? 'Solving...' : 'Solve'}</button>
+			<div className='flex gap-12 md:gap-16'>
+				<button type='button' disabled={isFilling} onClick={handleReset} className={`bg-[#242424] text-white py-2 px-3 md:py-3 md:px-4 text-sm md:text-base rounded-lg mt-4 hover:bg-[#333333] hover:scale-105 active:scale-110 disabled:bg-[#505050]`}>Reset</button>
+				<button type='button' onClick={handleFill} className='bg-[#242424] text-white py-2 px-3 md:py-3 md:px-4 text-sm md:text-base rounded-lg mt-4 hover:bg-[#333333] hover:scale-105 active:scale-110'>{isFilling ? 'Filling...' : 'Fill'}</button>
+				<button type='button' disabled={isFilling} onClick={handleSubmit} className={`bg-[#242424] text-white py-2 px-3 md:py-3 md:px-4 text-sm md:text-base rounded-lg mt-4 hover:bg-[#333333] hover:scale-105 active:scale-110 disabled:bg-[#505050]`}>{solving ? 'Solving...' : 'Solve'}</button>
 			</div>
-		</>
+		</div>
 	)
 }
 
